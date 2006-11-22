a33a7d4bb820d2587cf6f1ae0973712910831ccd
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2006-11-22 18:11:45 +0000

    tag V00-00-01: first import into CVS

---
+++ bin/FUShmClient.h
@@ -0,0 +1,45 @@
+#ifndef FUSHMCLIENT_H
+#define FUSHMCLIENT_H
+
+
+#include "EventFilter/ResourceBroker/interface/FUShmBuffer.h"
+
+#include <vector>
+
+
+namespace evf {
+  
+
+  class FUShmClient
+  {
+  public:
+    //
+    // construction/destruction
+    //
+    FUShmClient(FUShmBuffer* buffer);
+    ~FUShmClient();
+    
+    
+    //
+    // member functions
+    //
+    unsigned int readNext(std::vector<std::vector<unsigned char> > &feds);
+
+    double       crashPrb() const        { return crashPrb_; }
+    void         setCrashPrb(double prb) { crashPrb_=prb;    }
+    
+    
+  private:
+    //
+    // member data
+    //
+    FUShmBuffer* buffer_;
+    double       crashPrb_;
+
+  };
+  
+  
+} // namespace evf
+
+
+#endif
